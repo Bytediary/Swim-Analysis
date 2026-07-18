c25c30ae216a16a24c67dc503197d86348c55a96
Ria Chatterjee <ria.chatterjee2023@gmail.com>
2026-07-18 12:28:37 -0700

    Fixed Indexing

--- Swimming_Analysis.py
+++ Swimming_Analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt 
+
 df_swimming=pd.read_csv("Swim_Data.csv")
 #Add the condition for when it is a person's first time swimming that event and they won't have a seed time.
 df_swimming["Seed_Time_Seconds"]=df_swimming["Seed_Time_Seconds"].fillna("N/A(First Time)")
